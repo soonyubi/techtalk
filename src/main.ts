dc535e4ad245438c4cddb75e94dd9f6046c49f79
soonyubi <soonyubi1@naver.com>
2023-10-13 23:49:38 +0900

    remove

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// some test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
