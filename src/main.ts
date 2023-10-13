610d6b4b5b3495250efb6f353791543d6de6daaa
soonyubi <soonyubi1@naver.com>
2023-10-13 23:23:27 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// some test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
